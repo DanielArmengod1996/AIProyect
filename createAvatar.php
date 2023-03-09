ab9df275ecbff3c1eae75789b5c0755e58cd3d10
IdeaPool <30792292+DanielArmengod1996@users.noreply.github.com>
2023-03-09 13:29:19 +0000

    9 mar 2023, 14:29

--- createAvatar.php
+++ createAvatar.php
@@ -1,58 +1,39 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Avatar creator</title>
-	<!-- Incluir los archivos de CSS de Bootstrap -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<style type="text/css">
-		body {
-			margin: 0;
-			padding: 0;
-			font-family: Arial, sans-serif;
-			background: linear-gradient(to bottom, #888, #999);
-		}
-		h1 {
-			font-size: 3rem;
-			font-weight: 600;
-			text-transform: uppercase;
-			letter-spacing: 2px;
-			color: #333;
-			margin: 0;
-			padding: 1rem 0;
-			text-align: center;
-			border-bottom: 2px solid #333;
-		}
-		#avatar-container {
-			background-color: #fff;
-			padding: 20px;
-			border-radius: 20px;
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-			box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-			margin: 50px auto;
-			max-width: 600px;
-		}
-		#avatar-form {
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-			width: 100%;
-			margin-top: 20px;
-		}
-		#avatar-image {
-			width: 300px;
-			height: 300px;
-			object-fit: cover;
-			margin-bottom: 20px;
-		}
-		#avatar-submit {
-			margin-top: 20px;
-		}
-	</style>
-</head>
-<body>
-	<h1>Avatar creator. Create your avatar now!</h1>
+<!-- saved from url=(0052)https://getbootstrap.com/docs/4.0/examples/product/# -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="https://getbootstrap.com/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Product example for Bootstrap</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/product/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="product.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="site-header sticky-top py-1">
+      <div class="container d-flex flex-column flex-md-row justify-content-between">
+        <a class="py-2" href="https://getbootstrap.com/docs/4.0/examples/product/#">
+          📸
+        </a>
+        <a class="py-2 d-none d-md-inline-block" href="https://getbootstrap.com/docs/4.0/examples/product/#">Home</a>
+        <a class="py-2 d-none d-md-inline-block" href="https://getbootstrap.com/docs/4.0/examples/product/#">Create Avatar</a>
+        <a class="py-2 d-none d-md-inline-block" href="https://getbootstrap.com/docs/4.0/examples/product/#">About Us</a>
+        <a class="py-2 d-none d-md-inline-block" href="https://getbootstrap.com/docs/4.0/examples/product/#">Login</a>
+        <a class="py-2 d-none d-md-inline-block" href="https://getbootstrap.com/docs/4.0/examples/product/#">Register</a>
+      </div>
+    </nav>
+
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
 	<div id="avatar-container">
 		<form id="avatar-form" method="POST" enctype="multipart/form-data">
 			<img id="avatar-image" >
@@ -63,14 +44,74 @@
 			<button id="avatar-submit" type="submit" class="btn btn-primary">Create Avatar</button>
 		</form>
 	</div>
+    </div>
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+      <div class="col-md-5 p-lg-5 mx-auto my-5">
+        <h1 class="display-4 font-weight-normal">Create your own Avatar!</h1>
+        <p class="lead font-weight-normal">Create your own personalized avatar with AI</p>
+        <a class="btn btn-warning " href="https://localhost/avatario/avatar">Create NOW </a>
+      </div>
+      <div>PATATIFICO</div>
+    </div>
 
-	<!-- Incluir los archivos de JavaScript de Bootstrap -->
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <footer class="container py-5">
+      <div class="row">
+        <div class="col-12 col-md">
+          📸
+          <small class="d-block mb-3 text-muted">© 2022-2023</small>
+        </div>
+        <div class="col-6 col-md">
+          <h5>Features</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="https://getbootstrap.com/docs/4.0/examples/product/#">1. something</a></li>
+            <li><a class="text-muted" href="https://getbootstrap.com/docs/4.0/examples/product/#">2. something</a></li>
+            <li><a class="text-muted" href="https://getbootstrap.com/docs/4.0/examples/product/#">3. somtething</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5>Resources</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="https://getbootstrap.com/docs/4.0/examples/product/#">1. something</a></li>
+            <li><a class="text-muted" href="https://getbootstrap.com/docs/4.0/examples/product/#">2. something</a></li>
+            <li><a class="text-muted" href="https://getbootstrap.com/docs/4.0/examples/product/#">3. somtething</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5>Resources</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="https://getbootstrap.com/docs/4.0/examples/product/#">1. something</a></li>
+            <li><a class="text-muted" href="https://getbootstrap.com/docs/4.0/examples/product/#">2. something</a></li>
+            <li><a class="text-muted" href="https://getbootstrap.com/docs/4.0/examples/product/#">3. somtething</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5>About</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="https://getbootstrap.com/docs/4.0/examples/product/#">1. something</a></li>
+            <li><a class="text-muted" href="https://getbootstrap.com/docs/4.0/examples/product/#">2. something</a></li>
+            <li><a class="text-muted" href="https://getbootstrap.com/docs/4.0/examples/product/#">3. somtething</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
 
-	<script type="text/javascript">
-		// Previsualización de la imagen seleccionada
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="popper.min.js"></script>
+    <script src="bootstrap.min.js"></script>
+    <script src="holder.min.js"></script>
+    <script>
+      Holder.addTheme('thumb', {
+        bg: '#55595c',
+        fg: '#eceeef',
+        text: 'Thumbnail'
+      });
+
+	  		// Previsualización de la imagen seleccionada
 		var avatarImage = document.getElementById('avatar-image');
 		var avatarFileInput = document.getElementById('avatar-file');
 		avatarFileInput.addEventListener('change', function() {
@@ -81,6 +122,10 @@
 			}
 			reader.readAsDataURL(file);
 		});
-	</script>
-</body>
-</html>
+    </script>
+  	<!-- Incluir los archivos de JavaScript de Bootstrap -->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body></html>
