b4181cb1664d9b52717710d5fa2576e20f285946
IdeaPool <30792292+DanielArmengod1996@users.noreply.github.com>
2023-03-12 14:14:44 +0000

    12 mar 2023, 15:14

--- createAvatar.php
+++ createAvatar.php
@@ -102,7 +102,7 @@
 
           <?php
             require_once 'Discord/Discord.php';
-
+            require_once 'confFile.php';
             $discord = new Discord([
                 'token' => 'MTA3OTg3ODIyNTU2NTU4OTYyNQ.GrWXsI.TktJkjm7DPeKXPkCJkYERv3x5-LpYgx2DxExmY',
                 'intents' => Intents::getDefaultIntents()
