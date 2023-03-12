5127c4979a0499aa1311b7321550bb1aff09f46f
IdeaPool <30792292+DanielArmengod1996@users.noreply.github.com>
2023-03-12 21:10:33 +0000

    12 mar 2023, 22:10

--- createAvatar.php
+++ createAvatar.php
@@ -61,7 +61,10 @@
             
             include __DIR__.'vendor/autoload.php';
 
-         
+          // Import classes, install a LSP such as Intelephense to auto complete imports
+          use Discord\Discord;
+          use Discord\Parts\Channel\Message;
+          use Discord\WebSockets\Intents;
 
           // Create a $discord BOT
           $discord = new Discord([
