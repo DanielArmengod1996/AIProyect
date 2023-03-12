4dd0fdddee3b1efe8a8c0ed61da3f2f348cefcfe
IdeaPool <30792292+DanielArmengod1996@users.noreply.github.com>
2023-03-12 21:00:59 +0000

    12 mar 2023, 22:00

--- createAvatar.php
+++ createAvatar.php
@@ -61,28 +61,41 @@
             
             include __DIR__.'/vendor/autoload.php';
 
-            use Discord\Discord;
-            use Discord\Parts\Channel\Message;
-            use Discord\WebSockets\Intents;
-            use Discord\WebSockets\Event;
-            
-            $discord = new Discord([
-                'token' => $discordBotToken,
-                'intents' => Intents::getDefaultIntents()
-            //      | Intents::MESSAGE_CONTENT, // Note: MESSAGE_CONTENT is privileged, see https://dis.gd/mcfaq
-            ]);
-            
-            $discord->on('ready', function (Discord $discord) {
-                echo "Bot is ready!", PHP_EOL;
-            
-                // Listen for messages.
-                $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
-                    echo "{$message->author->username}: {$message->content}", PHP_EOL;
-                    // Note: MESSAGE_CONTENT intent must be enabled to get the content if the bot is not mentioned/DMed.
-                });
-            });
-            
-            $discord->run();
+          // Import classes, install a LSP such as Intelephense to auto complete imports
+          use Discord\Discord;
+          use Discord\Parts\Channel\Message;
+          use Discord\WebSockets\Intents;
+
+          // Create a $discord BOT
+          $discord = new Discord([
+              'token' => '', // Put your Bot token here from https://discord.com/developers/applications/
+              'intents' => Intents::getDefaultIntents() | Intents::MESSAGE_CONTENT // Required to get message content, enable it on https://discord.com/developers/applications/
+          ]);
+
+          // When the Bot is ready
+          $discord->on('init', function (Discord $discord) {
+
+              // Listen for messages
+              $discord->on('message', function (Message $message, Discord $discord) {
+
+                  // If message is from a bot
+                  if ($message->author->bot) {
+                      // Do nothing
+                      return;
+                  }
+
+                  // If message is "ping"
+                  if ($message->content == 'ping') {
+                      // Reply with "pong"
+                      $message->reply('pong');
+                  }
+
+              });
+
+          });
+
+          // Start the Bot (must be at the bottom)
+          $discord->run();
       }
     ?>
       <form method="post" action="">
