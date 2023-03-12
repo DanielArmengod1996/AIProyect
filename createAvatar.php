418215ba84e256906f2dd77657429fd1d5d1257d
IdeaPool <30792292+DanielArmengod1996@users.noreply.github.com>
2023-03-12 21:28:14 +0000

    12 mar 2023, 22:28

--- createAvatar.php
+++ createAvatar.php
@@ -55,37 +55,9 @@
         </ul>
         
         <?php
-		    if(isset($_POST['validarDiscord'])) {
-
-          require_once 'confFile.php';
-            
-
-          include __DIR__.'vendor/autoload.php';
-
-          use Discord\Discord;
-          use Discord\Parts\Channel\Message;
-          use Discord\WebSockets\Intents;
-          use Discord\WebSockets\Event;
-
-          $discord = new Discord([
-              'token' => $discordBotToken,
-              'intents' => Intents::getDefaultIntents()
-          //      | Intents::MESSAGE_CONTENT, // Note: MESSAGE_CONTENT is privileged, see https://dis.gd/mcfaq
-          ]);
-
-          $discord->on('ready', function (Discord $discord) {
-              echo "Bot is ready!", PHP_EOL;
-
-              // Listen for messages.
-              $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
-                  echo "{$message->author->username}: {$message->content}", PHP_EOL;
-                  // Note: MESSAGE_CONTENT intent must be enabled to get the content if the bot is not mentioned/DMed.
-              });
-          });
-
-          $discord->run();
-      }
-    ?>
+          if(isset($_POST['validarDiscord'])) {
+          }
+        ?>
       <form method="post" action="">
 		    <input type="submit" name="validarDiscord" value="Ejecutar función">
       </form>
